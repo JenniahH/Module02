4e20059b28ce843b601426d07eb5cb46de2bff93
JenniahH <32494602+JenniahH@users.noreply.github.com>
2017-10-03 23:23:16 -0400

    Add files via upload

---
+++ CalculationsV2.java
@@ -0,0 +1,58 @@
+
+/**
+ * Write a description of class CalculationsV2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CalculationsV2
+{
+    {
+// Addition
+        System.out.println("Addition");
+        System.out.println( 25 + 9 );
+        System.out.println( 43.21 + 3.14 + 5.0 );
+        //System.out.println();
+        
+        // Subtraction
+        System.out.println("Subtraction");
+        System.out.println( 16 - 9 - 25 );
+        System.out.println( 3.14 - 5.0 );
+        //System.out.println();
+        
+        // Multiplication
+        System.out.println("Multiplication");
+        System.out.println( 25 * 9 ); 
+        System.out.println(3.14 * 5.0 * 5.0 );
+        //System.out.println();
+        
+        // Division
+        System.out.println("Division");
+        System.out.println( 9 / 25 );
+        System.out.println( 43.21 / 5.0 );
+        //System.out.println();
+         
+        // Modulus operator
+        System.out.println("Modulus");
+        System.out.println( 16 % 9 );
+        System.out.println( 5.0 % 3.14 );
+        //System.out.println();
+        
+        // 2.02 Lab Equations
+        // 15 divided by 2.5 times -2 plus 10 / 5
+         System.out.println("-10");
+        // 234 minus (234 divided by 6 modulus 12) + 3
+         System.out.println("234");
+        // (46.2 divided by 11) minus 3 plus 24 modulus (17 minus 2 times 3)
+        System.out.println("3.2");
+        // 480 divided by 10 divided by 12 plus 200 *.5 minus 20 modulus 8
+        System.out.println("100");
+        // 2.03 Additional int Equations
+        //Addition system.out.println("Addition");
+        System.out.println("22 plus 4 equals" + (22+4));
+        System.out.println();
+        
+        //2.04 Additional double Equations
+        
+    }
+}
